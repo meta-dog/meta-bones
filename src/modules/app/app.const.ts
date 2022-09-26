1d066a1180c9555687a5a93f42745e19a1f332c6
David Camara Iglesias <david.c.iglesias@gmail.com>
2022-09-27 00:23:32 +0200

    🥅 Catch invalid referrals

--- src/modules/app/app.const.ts
+++ src/modules/app/app.const.ts
@@ -1,9 +1,10 @@
 export const MAX_PENDING_ATTEMPTS = 10;
 export const REFERRAL_BASE_URL = 'https://www.oculus.com/appreferrals';
 export const PLATFORM_BASE_URL = 'https://www.oculus.com/experiences';
+export const BROWSER_USER_AGENT =
+  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36';
 export const HEADERS = {
-  'User-Agent':
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36',
+  'User-Agent': BROWSER_USER_AGENT,
   'Accept-Language': 'en-GB,en;q=0.9',
   accept:
     'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
@@ -18,3 +19,5 @@ export const HEADERS = {
   'upgrade-insecure-requests': '1',
   dnt: '1',
 };
+
+export const DEFAULT_CRAWLER: 'axios' | 'puppeteer' = 'axios';

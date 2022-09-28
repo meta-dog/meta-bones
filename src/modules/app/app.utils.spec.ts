6c415333049d836e40826bdbb04f3afdf11b415c
David Camara Iglesias <david.c.iglesias@gmail.com>
2022-09-28 16:10:25 +0200

    👔 Update BL to allow "." on usernames

--- src/modules/app/app.utils.spec.ts
+++ src/modules/app/app.utils.spec.ts
@@ -75,7 +75,9 @@ test.each(validateAppIdData)(
 - Usernames must start with a letter or digit.
 - Usernames may be between 2 and 20 characters in length.
 - Usernames may include a combination of letters, digits, dashes and underscores, but may not include dashes or underscores consecutively.
-- Usernames may not have spaces, slashes or full stops.
+- Usernames may not have spaces, slashes or ~~full stops~~.
+
+- NOTE: Full stops seem to work
 
 */
 const validateAdvocateIdData: ValidateAdvocateIdData[] = [
@@ -88,15 +90,15 @@ const validateAdvocateIdData: ValidateAdvocateIdData[] = [
     output: true,
   },
   {
+    input: 'cant.stop.me.now',
+    output: true,
+  },
+  {
     input: '1',
     output: false,
   },
   {
     input: 'a',
-    output: false,
-  },
-  {
-    input: 'cant.stop.me.now',
     output: false,
   },
   {
--- src/modules/app/app.utils.ts
+++ src/modules/app/app.utils.ts
@@ -20,7 +20,7 @@ export class AdvocateIdConstraint implements ValidatorConstraintInterface {
   validate(advocate_id: string) {
     if (advocate_id === null) return false;
     const hasLengthAndOnlyValidChars =
-      advocate_id.match(/^[0-9a-zA-Z]{1}[\w\-_]+$/) !== null;
+      advocate_id.match(/^[0-9a-zA-Z]{1}[\w\.\-_]+$/) !== null;
     if (!hasLengthAndOnlyValidChars) return false;
     const hasDoubleDashOrUnderscore = advocate_id.match(/[-_]{2,}/);
     if (hasDoubleDashOrUnderscore) return false;
